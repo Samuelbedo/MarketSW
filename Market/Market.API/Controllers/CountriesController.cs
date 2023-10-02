c22c7e3333d1a99197416392d88de62ffcd381ee
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-10-02 14:48:43 -0500

    Gets for CountriesController createds

---
+++ Market/Market.API/Controllers/CountriesController.cs
@@ -0,0 +1,43 @@
+﻿using Market.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController (DataContext context)
+        {
+            _context = context;
+        }
+
+
+        //lista de paises
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+
+        //Get por parametro
+        [HttpGet("{Id=Int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync
+                (c => c.Id == id);
+            
+            if(country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+            
+        }
+    }
+}
